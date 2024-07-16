858b78c765eee3867e20dfa2b63b23258c55cc9a
j-o-bazely <j.o.bazely@qmul.ac.uk>
2024-07-16 10:27:45 +0100

    Ch1_Environment_Setup
    - Creating required directories
    
    - Creating files for:
       - Required modules
       - Tracking script order
       - Tracking files run
    
    - Creating required folders

---
+++ Ch1.RNA/Scripts/Ch1.R_requiredModules.sh
@@ -0,0 +1,9 @@
+#### Ch1_R Required Modules ####
+
+# Written by: James B
+
+# Date: 16.07.2024
+
+# A list of all modules required for shell scripts in the RNA data pipeline from 
+
+####    ####    ####
